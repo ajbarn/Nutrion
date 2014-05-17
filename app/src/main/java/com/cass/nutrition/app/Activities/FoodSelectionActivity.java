f04753be82425ae42bee3bcd3d938a1e4376007f
Alex Barnett <abarnett14@gmail.com>
2014-05-17 17:39:58 -0400

    Added an autoComplete box
    added an autocomplete box

--- app/src/main/java/com/cass/nutrition/app/Activities/FoodSelectionActivity.java
+++ app/src/main/java/com/cass/nutrition/app/Activities/FoodSelectionActivity.java
@@ -3,10 +3,15 @@ package com.cass.nutrition.app.Activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cass.nutrition.app.Adapters.StableArrayAdapter;
 import com.cass.nutrition.app.R;
@@ -18,7 +23,7 @@ import java.util.ArrayList;
  */
 public class FoodSelectionActivity extends Activity {
 
-    private final String[] tempFood = {"Apple", "Banana", "Cheese", "Dog Food", "Elephant Steak", "Fish", "Granola", "Hockey Tape", "Indian Butter Chicken", "Jube- jubes","Kilograms" };
+    private final String[] tempFood = {"Apple", "Banana", "Cheese", "Dog Food", "Elephant Steak", "Fish", "Granola", "Hockey Tape", "Indian Butter Chicken", "Jube- jubes","Kilograms", "Kilos", "Kill", "kites", "kangaroos" };
     private final ArrayList<String> tempFoodList = new ArrayList<String>();
 
     @Override
@@ -34,7 +39,11 @@ public class FoodSelectionActivity extends Activity {
                 android.R.layout.simple_list_item_1, tempFoodList);
         listView.setAdapter(adapter);
 
-
+        ArrayAdapter<String> textAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, tempFoodList);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.search_bar);
+        textView.setAdapter(textAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -48,4 +57,24 @@ public class FoodSelectionActivity extends Activity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu_food_selection, menu);
+            return true;
+        }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.settings:
+                Log.d("ASDF", "settings clicked");
+                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Log.d("ASDF", "Don't know how you ended up here");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
