bc13371366c169de1dfb5f74e5c434322dffa2f0
CassEvans <cass.evans@utoronto.ca>
2014-05-17 14:20:24 -0400

    Update Text on MainActivity
    FIRE

--- app/src/main/java/com/cass/nutrition/app/MainActivity.java
+++ app/src/main/java/com/cass/nutrition/app/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            Log.d("CASS", "selected settings");
+            Log.d("FIRE", "selected settings");
             return true;
         }
         return super.onOptionsItemSelected(item);
